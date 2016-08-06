25e257ec8073875eba44a13fc57fe279bcfeec79
Ed Daughtery <ed.daughtery@gmail.com>
2016-08-06 12:21:15 -0400

    Populated the Quaterinion to DCM function

--- src/Utils.jl
+++ src/Utils.jl
@@ -290,12 +290,26 @@ end
 
 function QuatToDCM(quat)
 
-    q0 = quat[0];
-    q1 = quat[1];
-    q2 = quat[2];
-    q3 = quat[3];
+    q0 = quat[1];
+    q1 = quat[2];
+    q2 = quat[3];
+    q3 = quat[4];
     
-    return quatdot;
+    Tr_bi = zeros(3,3);
+    Tr_bi[1,1] = q0^2 + q1^2 - q2^2 - q3^2;
+    Tr_bi[2,2] = q0^2 - q1^2 + q2^2 - q3^2;
+    Tr_bi[3,3] = q0^2 - q1^2 - q2^2 + q3^2;
+    
+    Tr_bi[2,1] = 2.0*(q1*q2 - q0*q3);
+    Tr_bi[1,2] = 2.0*(q1*q2 + q0*q3);
+    
+    Tr_bi[3,1] = 2.0*(q1*q3 + q0*q2);
+    Tr_bi[1,3] = 2.0*(q1*q3 - q0*q2);
+    
+    Tr_bi[3,2] = 2.0*(q2*q3 - q0*q1);
+    Tr_bi[2,3] = 2.0*(q2*q3 + q0*q1);
+    
+    return Tr_bi;
 end
 
 ###########################################################################
